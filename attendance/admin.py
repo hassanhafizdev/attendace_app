d9380448d5de18f048333fdada217a8a14438512
Hassan Javed <hassanhafizdev@gmail.com>
2019-07-08 11:31:25 +0500

    registered models

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import(
+    Gender, Teacher, Student, StudentAttendance, TeacherAttendace
+)
 # Register your models here.
+
+admin.site.register(Gender)
+admin.site.register(Teacher)
+admin.site.register(Student)
+admin.site.register(StudentAttendance)
+admin.site.register(TeacherAttendace)
